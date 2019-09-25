f81c0a6dcff5a558957afa48dd9efc5775c9bdfc
nulltiply <49563920+nulltiply@users.noreply.github.com>
2019-09-25 22:41:18 +0200

    index flags in array by value returned from flags
    TCP flags are now indexed in an array by the value they return from the tcp_flags_field.
    Added additional flags and replaced if statement clutter.

--- null_scan/null_scan.lua
+++ null_scan/null_scan.lua
@@ -1,14 +1,16 @@
 -- needs to be outside function called from menu
 -- grabs the tcp.stream and tcp flags fields
 local tcp_stream_info = Field.new("tcp.stream");
-local tcp_flags = Field.new("tcp.flags")
+local tcp_flags_field = Field.new("tcp.flags")
+
+-- Scans to test against
+-- Null scan (-sN) - <None> - returns flag value of 0/0x000 
+-- FIN scan (-sF) - FIN - returns flag value of 1/0x01
+-- Xmas scan (-sX) - FIN-PSH-URG - returns flag value of 41/0x029 
 
 -- TCP FLAGS
-local tcp_fin = 1 -- 0x01
-local tcp_syn = 2 -- 0x02
-local tcp_rst = 4 -- 0x04
-local tcp_ack = 16 -- 0x10
-local tcp_syn_ack = 18 -- 0x12
+local tcp_flags = { [0] = "NONE", [1] = "FIN", [2] = "SYN", [4] = "RST", [8] = "PSH", [16] = "ACK", [17] = "FIN-ACK", [18] = "SYN-ACK",
+[20] = "RST-ACK", [24] = "PSH-ACK", [25] = "FIN-PSH-ACK", [32] = "URG", [41] = "FIN-PSH-URG"}
 
 function tcp_stream()
 
@@ -25,45 +27,29 @@ function tcp_stream()
     --  call remove() function when closing window
     tw:set_atclose(remove)
     
-    -- tw:append('Debug: before tap.packet');
-
     -- will be run on every packet 
     -- last_index = 0
 
     function tap.packet(pinfo,tvb)
         local tcp_stream_index = tcp_stream_info();
-        -- null check, lua... "nil"
-        if tcp_stream_index ~= nil then
+        -- null/nil check
+        if (tcp_stream_index ~= nil) then
 
             -- extract source, destination IP from packets
             local src = tostring(pinfo.src) or 0
             local dst = tostring(pinfo.dst)or 0
             -- extract tcp flags in hex
-            local flags = tcp_flags()
+            local flags = tcp_flags_field()
             if flags then
                 local flags_formatted = string.format("tcp.flags = %#x", flags.value)
-                -- "Frame no: " .. pinfo.number
                 local packet_item = "TCP Index: " .. tostring(tcp_stream_index) .. " Frame No: " .. pinfo.number 
-                local tcp_flag=""
-                if (flags.value == tcp_fin) then
-                    tcp_flag = "FIN"
-                end
-                if (flags.value == tcp_syn) then
-                    tcp_flag = "SYN"
-                end
-                if (flags.value == tcp_ack) then
-                    tcp_flag = "ACK"
-                end
-                if (flags.value == tcp_syn_ack) then
-                    tcp_flag = "SYN-ACK"
-                end
-                if (flags.value == tcp_rst) then
-                    -- tw:append("Flag: RST flag\r\n")
-                    tcp_flag = "RST"
-                end
+                local set_tcp_flags = flags.value
 
+                if (flags.value ~= nil) then
+                    set_tcp_flags = tcp_flags[flags.value]
+                end
                 -- lua arrays start on 1
-                packet_item = packet_item .. " Flag: " .. tcp_flag .. "\r\n"
+                packet_item = packet_item .. " Flag: " .. set_tcp_flags .. "\r\n"
                 streams_unsorted[iterator] = packet_item
                 iterator = iterator + 1
                 -- tw:append(packet_item)
@@ -86,8 +72,6 @@ function tcp_stream()
                 if (index_num > tcp_index_max) then
                     tcp_index_max = index_num
                 end
-                -- tw:append("Got from string" .. index_num .. " :s: ")
-                -- tw:append(item);
                 
             end
             
@@ -95,23 +79,28 @@ function tcp_stream()
             for i=0, tcp_index_max do  
                 tw:append("TCP Stream Index: " .. i .. "\r\n")
                 nested_iterator = 1
-                streams_sorted[i] = {}          
+                streams_sorted[i] = {}        
+
+                tcp_stream_length = 0
                 for j, item in pairs(streams_unsorted) do
                     index_num = string.match(item, "%d+")
                     index_num = tonumber(index_num)
+                    
                     if (i == index_num) then
                         streams_sorted[i][nested_iterator] = item
                         nested_iterator = nested_iterator + 1
                         tw:append("\t" .. item)
+                        tcp_stream_length = tcp_stream_length + 1
                     end
-                end 
+                end
+                tw:append("TCP Stream Length: " .. tcp_stream_length .. "\r\n") 
             end
+            -- last iteration to check for possible scans?
+            -- get length here instead table.getn(array) 
 
 		end
     end
     
-    -- tw:append('Debug: after tap.packet');
-
     -- this function will be called whenever a reset is needed
 	-- for instance when reloading the capture file
     function tap.reset()
@@ -123,4 +112,4 @@ function tcp_stream()
 end
 
 -- assigns function and menu option as Tools-->Null-tools-->TCP-Stream
-register_menu("Null-Scan/TCP-Stream", tcp_stream, MENU_TOOLS_UNSORTED)
+register_menu("Null-Tools/TCP-Stream", tcp_stream, MENU_TOOLS_UNSORTED)
